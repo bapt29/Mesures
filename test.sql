d597688ef0f513da0443b6b4cc2cd57f003df2a5
Baptiste Cabiten <baptiste.cabiten@hotmail.com>
2017-04-05 16:32:04 +0200

    Added missing sensors

--- test.sql
+++ test.sql
@@ -8,7 +8,9 @@ CREATE TABLE `Sensors` (
 	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	`name`	TEXT
 );
-INSERT INTO `Sensors` (id,name) VALUES (1,'humidity');
-INSERT INTO `Sensors` (id,name) VALUES (2,'soil_temperature');
-INSERT INTO `Sensors` (id,name) VALUES (3,'soil_moisture');
-INSERT INTO `Sensors` (id,name) VALUES (4,'raindrop');
+INSERT INTO `Sensors` (id,name) VALUES (1,'air_humidity');
+INSERT INTO `Sensors` (id,name) VALUES (2,'air_temperature');
+INSERT INTO `Sensors` (id,name) VALUES (3,'air_humidity');
+INSERT INTO `Sensors` (id,name) VALUES (4,'soil_temperature');
+INSERT INTO `Sensors` (id,name) VALUES (5,'soil_moisture');
+INSERT INTO `Sensors` (id,name) VALUES (6,'raindrop');
