beb199d6b02fefcd906741d43603c5f00844576f
Baptiste Cabiten <baptiste.cabiten@hotmail.com>
2017-06-09 16:08:25 +0200

    read_config update

--- configuration.py
+++ configuration.py
@@ -11,8 +11,12 @@ class Configuration:
         if configuration_path is not None and os.path.exists(configuration_path):
             os.chdir(configuration_path)
 
-    def read_config(self, section):
+    def read_config(self, section, option=None):
         self.__config.read(self.__configuration_name)
+
+        if option is not None:
+            if self.__config.has_option(section, option):
+                return self.__config[section][option]
 
         return dict(self.__config.items(section))
 
